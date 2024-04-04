f76744fd75d8b8d08d97b8a67854fb2441fae2d0
Noah <noah4882@163.com>
2024-04-04 16:37:39 +0800

    feat(vue): 支持 jsx

--- packages/eslint-config-vue/src/index.ts
+++ packages/eslint-config-vue/src/index.ts
@@ -8,6 +8,9 @@ export = {
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   extends: [
     'eslint:recommended',
